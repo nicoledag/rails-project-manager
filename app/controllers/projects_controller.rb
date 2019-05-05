55ff139a88055d67a08611cd6d87362df61af7b9
github username <github email address>
2019-05-04 21:29:32 -0400

    Solve client/project/new action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,15 +3,17 @@ class ProjectsController < ApplicationController
   def index
   end
 
-  def new
-    # raise params.inspect
-    if params[:client_id] && client = Client.find_by_id(params[:client_id])
-      @project = client.projects.build
-      # @project = current_user.projects.create(client: client)
-    else
+
+    def new
+        # raise params.inspect
+      @client = Client.find(params[:client_id])
       @project = Project.new
     end
-  end
+
+  #   if params[:client_id] && @client = Client.find_by_id(params[:client_id])
+  #     @project = current_user.projects.create(client: client)
+  #   end
+  # end
 
   def create
     raise params.inspect
