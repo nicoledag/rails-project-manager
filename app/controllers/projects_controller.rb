d26167c1819940cc4c2a93bad2c4745422ee0959
github username <github email address>
2019-05-05 09:25:06 -0400

    Clean up comments in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,19 +5,15 @@ class ProjectsController < ApplicationController
   end
 
 
-    def new
+  def new
         # raise params.inspect
-      if (params[:client_id]) &&  @client = Client.find_by_id(params[:client_id])
-        @project = @client.projects.build
-      else
-        @project = Project.new
-      end
+    if (params[:client_id]) &&  @client = Client.find_by_id(params[:client_id])
+      @project = @client.projects.build
+    else
+      @project = Project.new
     end
+  end
 
-  #   if params[:client_id] && @client = Client.find_by_id(params[:client_id])
-  #     @project = current_user.projects.create(client: client)
-  #   end
-  # end
 
   def create
     # raise params.inspect
@@ -25,10 +21,10 @@ class ProjectsController < ApplicationController
    # if @project.save
 
     if (params[:client_id])
-    @client = Client.find(params[:client_id])
-    @project = @client.projects.create(project_params)
-    @project.user_id = current_user.id
-    @project.save
+      @client = Client.find(params[:client_id])
+      @project = @client.projects.create(project_params)
+      @project.user_id = current_user.id
+      @project.save
 
       redirect_to client_project_path(@client, @project)
     elsif
@@ -49,7 +45,7 @@ class ProjectsController < ApplicationController
     @project = Project.find_by(id: params[:id])
   end
 
-  private
+private
   def project_params
     params.require(:project).permit(:name, :description)
   end
