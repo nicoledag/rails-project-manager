13a149607b2c0c0b30ad1396ff9e83e1ad7996e7
github username <github email address>
2019-05-08 13:23:31 -0400

    Add custom project validation on date

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,13 @@ class Project < ApplicationRecord
   has_many :comments
 
   validates :name, :description, :client, presence: true
+  validate :target_date_cannot_be_greater_than_completion_date
+
+  def target_date_cannot_be_greater_than_completion_date
+    if target_completion_date > completion_date
+      errors.add(:target_completion_date, "can't be greater than completion date")
+    end
+  end
 
   def format_target_date
     target_completion_date.strftime("%m/%d/%Y")
