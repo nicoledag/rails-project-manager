0e915cd704f8a8a2b03bcc731e3fae135b93d1a0
github username <github email address>
2019-05-08 12:14:28 -0400

    took out comments_attributes method in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,17 +7,5 @@ class Project < ApplicationRecord
   validates :name, :description, :client, presence: true
 
 
-  def comments_attributes=(comments_attributes)
-    comments_attributes.each do |i, comment_attributes|
-      # raise params.inspect
-      # @comment = Comment.find_or_create_by(content: comment_attributes[:content])
-        # if @comment.project_id == nil
-          self.comments.build(comment_attributes)
-        # else
-        #   self.comments.update(comment_attributes)
-        # end
-    end
-  end
-
 
 end
