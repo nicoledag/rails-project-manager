bb21cbca73243d1510167c00977b1f8a755d6863
github username <github email address>
2019-05-08 11:36:18 -0400

    Complete comment update action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,20 +11,37 @@ class CommentsController < ApplicationController
 
   def create
     # raise params.inspect
-      (params[:comment][:project_id])
-      @project = Project.find(params[:comment][:project_id])
-      @client = @project.client_id
-      @comment = @project.comments.create(comment_params)
-    if @comment.save
+    (params[:comment][:project_id])
+    @project = Project.find(params[:comment][:project_id])
+    @client = @project.client_id
+    @comment = @project.comments.create(comment_params)
 
-      redirect_to client_project_path(@client, @project)
-    else
-      render :new  #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
-    end
+      if @comment.save
+        redirect_to client_project_path(@client, @project)
+      else
+        render :new  #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
+      end
   end
 
   def show
+    #not using show action
+  end
+
+  def edit
+    # raise params.inspect
     set_comment
+  end
+
+  def update
+    # raise params.inspect
+    set_comment
+    @project = @comment.project
+    @client = @project.client
+    if project_user_equals_current_user && @comment.update(comment_params)
+      redirect_to client_project_path(@client, @project)
+    else
+      render :edit #allows for field with errors.
+    end
   end
 
   private
@@ -34,6 +51,10 @@ class CommentsController < ApplicationController
     if !@comment
       redirect_to projects_path
     end
+  end
+
+  def project_user_equals_current_user
+    @project.user == current_user
   end
 
   def comment_params
