7409ad11cb30cb2c99007aa453b7a40815bd23a2
github username <github email address>
2019-05-07 21:48:35 -0400

    Add comments controller create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,23 @@ class CommentsController < ApplicationController
   end
 
   def new
-
-
     (params[:project_id]) && @project = Project.find_by_id(params[:project_id])
     # raise params.inspect
     @comment = @project.comments.build
-
-
   end
 
   def create
-    raise params.inspect
+    # raise params.inspect
+    if (params[:comment][:project_id])
+      project = Project.find(params[:comment][:project_id])
+      @comment = project.comments.create(comment_params)
+      redirect_to projects_path
+    else
+      render :new  #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :project_id)
   end
 end
