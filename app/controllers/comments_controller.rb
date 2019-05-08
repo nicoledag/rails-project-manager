02fd5317786d55dccd0562b0123a6c2f4c7308f2
github username <github email address>
2019-05-07 23:06:53 -0400

    Add client project nested link to create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,19 +4,33 @@ class CommentsController < ApplicationController
   end
 
   def new
-    (params[:project_id]) && @project = Project.find_by_id(params[:project_id])
+    (params[:project_id]) && project = Project.find_by_id(params[:project_id])
     # raise params.inspect
-    @comment = @project.comments.build
+    @comment = project.comments.build
   end
 
   def create
     # raise params.inspect
     if (params[:comment][:project_id])
-      project = Project.find(params[:comment][:project_id])
-      @comment = project.comments.create(comment_params)
-      redirect_to projects_path
+      @project = Project.find(params[:comment][:project_id])
+      @client = @project.client_id
+      @comment = @project.comments.create(comment_params)
+      redirect_to client_projects_path(@client, @project)
     else
       render :new  #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
+    end
+  end
+
+  def show
+    set_comment
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+    if !@comment
+      redirect_to projects_path
     end
   end
 
