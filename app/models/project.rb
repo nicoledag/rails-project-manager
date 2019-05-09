3c75b52fa7165a912a8e381d51501f59903d91b2
github username <github email address>
2019-05-09 09:40:35 -0400

    Add project status attribute and user admin attribute

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,20 @@ class Project < ApplicationRecord
   validates :name, :description, :client, presence: true
   validate :target_date_cannot_be_greater_than_completion_date
 
+  STATUS = {
+    :incomplete => 0,
+    :complete => 1
+  }
+
+  def complete?
+    self.status = STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status = STATUS[:incomplete]
+  end
+
+
   def target_date_cannot_be_greater_than_completion_date
     if target_completion_date > completion_date
       errors.add(:target_completion_date, "can't be greater than completion date")
@@ -15,13 +29,13 @@ class Project < ApplicationRecord
 
   def format_target_date
     if target_completion_date
-      target_completion_date.strftime("%m/%d/%Y")
+      self.target_completion_date.strftime("%m/%d/%Y")
     end
   end
 
   def format_completed_date
     if completion_date
-      completion_date.strftime("%m/%d/%Y")
+      self.completion_date.strftime("%m/%d/%Y")
     end
   end
 
