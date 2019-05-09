51738ee416f242b30927b0d6497824389d515b56
github username <github email address>
2019-05-09 10:45:30 -0400

    Remove status from projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_133243) do
+ActiveRecord::Schema.define(version: 2019_05_09_144338) do
 
   create_table "clients", force: :cascade do |t|
     t.string "email"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2019_05_09_133243) do
     t.datetime "updated_at", null: false
     t.date "target_completion_date"
     t.date "completion_date"
-    t.integer "status", default: 0
   end
 
   create_table "users", force: :cascade do |t|
