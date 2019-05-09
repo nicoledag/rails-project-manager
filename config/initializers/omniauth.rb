2a37158845258d26644c399ff4a9dd88b933c718
github username <github email address>
2019-05-09 13:43:40 -0400

    Add note

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '65162a0382ccc7676a64', 'a0e42f4047fe7068a1b61345e7b96d4300393768'
 end
+
+
+#can use Figero gem and dotenv gem to hide keys
