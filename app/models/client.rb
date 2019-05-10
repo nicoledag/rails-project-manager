e1910bca82d0a0fed84c6e8d13f48b62be65232d
github username <github email address>
2019-05-10 10:37:48 -0400

    Update notes

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,9 +4,8 @@ class Client < ApplicationRecord
   has_many :users, through: :projects
 
   validates :company_name, presence: true
-  
-  # validates :company_name, uniqueness: true
   validates_uniqueness_of :company_name, :case_sensitive => false
-  # validates :phone, :zip, numericality: { only_integer: true }
+
+
 
 end
