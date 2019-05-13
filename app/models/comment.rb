ddb499ee831beb1f6b73fcf786ef2e02c34b93e0
github username <github email address>
2019-05-13 10:42:44 -0400

    Format comment created date attribute

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :project
 
   validates :content, presence: true
+
+
+  def format_date_created
+      self.created_at.strftime("%m/%d/%Y")
+  end
+
+
 end
