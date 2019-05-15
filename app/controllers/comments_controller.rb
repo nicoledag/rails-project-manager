0646d468296fa7a8534ee6661189b17037da1b72
github username <github email address>
2019-05-14 22:51:26 -0400

    Add note

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,10 +11,10 @@ class CommentsController < ApplicationController
 
   def create
     # raise params.inspect
-    @project = Project.find(params[:comment][:project_id])
-    set_client_instance_variable_for_redirect
-    if project_user_equals_current_user && @comment = @project.comments.create(comment_params)
+    @project = Project.find(params[:comment][:project_id])  #Project already exists..not building new project so not passing project_id through params.
+     if project_user_equals_current_user && @comment = @project.comments.create(comment_params)
       if @comment.save
+        set_client_instance_variable_for_redirect
         redirect_to client_project_path(@client, @project)
       else
         render :new  #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
